c6c02b7af7514f47efa3c8542785a25a932eb480
erankova <elinarankova@gmail.com>
2024-05-13 13:28:47 -0400

    env issues

--- src/deployment.py
+++ src/deployment.py
@@ -50,7 +50,7 @@ st.write("Current directory:", os.getcwd())
 st.write("Files in current directory:", os.listdir(os.getcwd()))
 
 # Print model directory
-model_dir = os.path.join(os.getcwd(),'cd','model')
+model_dir = os.path.join(os.getcwd(),'model')
 st.write("model directory:", os.listdir(model_dir))
 
 # Try going one directory up and listing files there
